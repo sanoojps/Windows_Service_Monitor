ec5e66fd2d6496bb831e1e16635a44a8ad2af3c0
mar Jinn <marjinn@live.com>
2013-02-26 07:51:21 +0530

    renamed all class initialisation variables to an '_' underscored verson
    Tested and working fine

--- Windows_Service_Monitor/WindowsServiceMonitor.cs
+++ Windows_Service_Monitor/WindowsServiceMonitor.cs
@@ -10,10 +10,10 @@ namespace Windows_Service_Monitor
     public class WindowsServiceMonitor
     {
          #region WMIQueryInitialSettings
-             private static ConnectionOptions connectionOptions;
+             private static ConnectionOptions _connectionOptions;
              //ManagementScope required if a WMI query is also required to be made
-             private static ManagementScope scope;
-             private static ManagementEventWatcher eventWatcher;
+             private static ManagementScope _scope;
+             private static ManagementEventWatcher _eventWatcher;
              
              private static string ComputerName = @"\\.";
              private static string Namespace = @"\root\cimv2";
@@ -30,22 +30,22 @@ namespace Windows_Service_Monitor
         public WindowsServiceMonitor()
         { 
             //get ConnectionOptions to set the appropriate Impersonation levels
-             connectionOptions = new ConnectionOptions();
+             _connectionOptions = new ConnectionOptions();
              
             //set the Impersonation level to Impersonate to 
              //assume current Logged in User's Privileges
-             connectionOptions.Impersonation = ImpersonationLevel.Impersonate;
+             _connectionOptions.Impersonation = ImpersonationLevel.Impersonate;
             
             //set the scope for the connection ie the Class Path 
-            scope = new ManagementScope(ManagementPath, connectionOptions);
+            _scope = new ManagementScope(ManagementPath, _connectionOptions);
 
             //create a ManagementEvent Watcher
-            eventWatcher =
+            _eventWatcher =
            new ManagementEventWatcher(ManagementPath, wqlQueryString);
 
             //Handle the EventArrived event using the EventArrivedEventHandler
 
-            eventWatcher.EventArrived +=
+            _eventWatcher.EventArrived +=
                new EventArrivedEventHandler(this.OnEventArrived);
             //OnEventArrived - the event handler function
             // private void OnEventArrived(object sender, 
@@ -60,7 +60,7 @@ namespace Windows_Service_Monitor
         public void StartMonitor()
         {
             // Start listening for events
-            eventWatcher.Start();
+            _eventWatcher.Start();
             // Do something while waiting for events
             System.Threading.Thread.Sleep(10000);
 
@@ -70,8 +70,8 @@ namespace Windows_Service_Monitor
         //and Dispose the eventWatcher object
         public void Dispose()
         {
-            eventWatcher.Stop();
-            eventWatcher.Dispose();
+            _eventWatcher.Stop();
+            _eventWatcher.Dispose();
         }
 
 
